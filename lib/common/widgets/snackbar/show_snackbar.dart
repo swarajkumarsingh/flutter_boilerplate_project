1312728212033e79185a087cc614f27d10a66390
swaraj <sswaraj169@gmail.com>
2023-07-11 11:57:40 +0530

    added sucess_snackbar.dart

---
+++ lib/common/widgets/snackbar/show_snackbar.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import '../../../my_app.dart';
+
+void showSnackBar(String message) {
+  final SnackBar snackBar = SnackBar(content: Text(message));
+  scaffoldMessengerKey.currentState?.showSnackBar(snackBar);
+}
