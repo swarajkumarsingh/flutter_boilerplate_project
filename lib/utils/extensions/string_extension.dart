35e40c8f4705d6bf3062ed3ee62b9acde6646f8c
swaraj <sswaraj169@gmail.com>
2023-07-31 21:01:53 +0530

    added func shortenForPrint

--- lib/utils/extensions/string_extension.dart
+++ lib/utils/extensions/string_extension.dart
@@ -6,8 +6,20 @@ extension StringExtension on String {
     return emailRegex.hasMatch(this);
   }
 
-    String get capitalize =>
+  String get capitalize =>
       isEmpty ? '' : '${this[0].toUpperCase()}${substring(1)}';
+}
+
+extension Prettify on String {
+  /// Shortens a long string by taking the start n chars (5 is default)
+  /// and the last n chars (if long enough). Useful for logging/printing.
+  String shortenForPrint([int n = 5]) {
+    if (length <= 2 * n) {
+      return this;
+    } else {
+      return '${substring(0, n)}...${substring(length - n)}';
+    }
+  }
 }
 
 extension CStringExtension on String? {
