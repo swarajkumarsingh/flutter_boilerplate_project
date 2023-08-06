0c1d9b4b94a0c1ab0352ef94e60a7b0b14e1ad42
swaraj <sswaraj169@gmail.com>
2023-08-06 09:05:54 +0530

    optimized restart app feature

--- lib/utils/restart/restart_impl.dart
+++ lib/utils/restart/restart_impl.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:restart_app/restart_app.dart';
 
-import '../../my_app.dart';
+import '../../constants/constants.dart';
 import '../../provider/generic_data_provider.dart';
 import 'restart.dart';
 import 'restart_widget.dart';
@@ -9,7 +11,7 @@ import 'restart_widget.dart';
 class RestartClassImpl extends RestartClass {
   @override
   Future<bool?> restart() async {
-    final bool result  = await Restart.restartApp();
+    final bool result = await Restart.restartApp();
     if (!result) {
       throw "Unable to restart the app";
     }
@@ -18,16 +20,31 @@ class RestartClassImpl extends RestartClass {
 
   @override
   void silentRestart([BuildContext? context]) {
-    if (context != null) {
-      RestartWidget.restartApp(context);
+    int numOfRetries = 0;
+
+    // Some errors occurs every time, and in that case restart app goes to an infinite loop, to prevent that we take a track of restart and if it exceeds the number, we exit the app
+    try {
+      if (numOfRetries == maxNumberOfRestartTries ||
+          numOfRetries > maxNumberOfRestartTries) {
+        exit(0);
+      }
+
+      if (context != null) {
+        RestartWidget.restartApp(context);
+        numOfRetries++;
+        return;
+      }
+      final BuildContext? secondaryContext = genericDataProvider.getContext();
+      if (secondaryContext != null) {
+        RestartWidget.restartApp(secondaryContext);
+        numOfRetries++;
+        return;
+      }
+      numOfRetries++;
+      restart();
       return;
+    } catch (e) {
+      exit(0);
     }
-    final BuildContext? secondaryContext = genericDataProvider.getContext();
-    if (secondaryContext != null) {
-      RestartWidget.restartApp(secondaryContext);
-      return;
-    }
-    runApp(const MyApp());
-    return;
   }
 }
